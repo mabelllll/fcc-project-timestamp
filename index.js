f21927d5374fbb3ada9b4d27c3f5893cbed297f3
Mabel Lee <leemabel.711@gmail.com>
2022-08-12 12:05:55 +0200

    complete task with all tests passing

--- index.js
+++ index.js
@@ -24,9 +24,52 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:date?", (req, res) => {
+  //console.log("date: " + req.params.date)
+  let input = req.params.date
+  let myRegex = /^[0-9]+$/
+  let d = new Date(input)
+  
+  if (input) {
+    if (myRegex.test(input)) {
+      // console.log("regex")
+      // console.log({
+      //   unix: input,
+      //   utc: new Date(input).toUTCString()
+      // } + "   " + (typeof input))
+      input = parseInt(input)
+      res.json({
+        unix: input,
+        utc: new Date(parseInt(input)).toUTCString()
+      })
+    } else {
+      if (d.toString() !== "Invalid Date") {
+        //console.log("correct date str")
+        res.json({
+          unix: d.getTime(),
+          utc: d.toUTCString()
+        })
+      } else {
+        // console.log("err")
+        res.json({
+          error: "Invalid Date"
+        })
+      }
+    }
+  } else {
+    let currentDate = Date.now()
+    // console.log("empty date")
+    res.json({
+      unix: new Date(currentDate).getTime(),
+      utc: new Date(currentDate).toUTCString()
+    })
+  }
+})
 
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+
